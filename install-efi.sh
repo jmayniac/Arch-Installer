a6d831c6aa51f181d14702ee6101740006017fc2
jmayniac <66429006+jmayniac@users.noreply.github.com>
2020-07-14 10:41:34 -0600

    Update install-efi.sh

--- install-efi.sh
+++ install-efi.sh
@@ -22,27 +22,40 @@ mkfs.ext4 /dev/sda2
 mkfs.fat -F 32 /dev/sda1
 #mount boot drive
 mount /dev/sda2 /mnt
+echo "Server = http://mirrors.gigenet.com/archlinux/$repo/os/$arch" > /etc/pacman.d/mirrorlist
 #install pacstrap
-pacstrap /mnt base linux linux-firmware
+pacstrap /mnt base linux linux-firmware nano sudo
 #genfstab
 genfstab -U /mnt >> /mnt/etc/fstab
 #chroot
-arch-chroot /mnt
-#update hostname file
-echo host >> /etc/hostname
+arch-chroot /mnt /bin/bash
+sed -i "/#en_US.UTF-8/s/^#//g" /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
+ln -sf /usr/share/zoneinfo/America/Denver /etc/localtime
+hwclock --systohc --localtime
+echo archpc >> /etc/hostname
 #update hosts file
-echo -e "127.0.0.1 \tlocalhost\n::1 \t\tlocalhost\n127.0.1.1 \thost" >> /etc/hosts
+echo -e "127.0.0.1 \tlocalhost\n::1 \t\tlocalhost\n127.0.1.1 \tarchpc" >> /etc/hosts
 #echo 127.0.0.1 localhost ::1 localhost 127.0.1.1 host >> /etc/hosts
 #update pacman
 pacman -Syu --noconfirm
 #network manager
 pacman -S networkmanager --noconfirm
+systemctl enable NetworkManager.service
 #mkinitcpio
 mkinitcpio -P
-#set up grub
-pacman -S grub efibootmgr --noconfirm
+echo "User password"
+useradd -m -G wheel,users -s /bin/bash john
+passwd john
+echo "Root password"
+passwd
+
 mkdir /boot/efi
 mount /dev/sda1 /boot/efi
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
-grub-mkconfig -o /boot/grub/grub.cfg
-systemctl enable NetworkManager.service
+
+
+
+exit
+umount -R /mnt
+reboot
